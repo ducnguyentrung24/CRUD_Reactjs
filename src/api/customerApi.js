580239da2298822c47e167e71a0b4a000bffcb30
ducnguyentrung24 <2251061745@e.tlu.edu.vn>
2026-01-15 23:18:19 +0700

    add .env

--- src/api/customerApi.js
+++ src/api/customerApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/customers";
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getAllCustomers = () => {
   return axios.get(API_URL);
